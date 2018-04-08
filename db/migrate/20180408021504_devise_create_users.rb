b3aceaa554f008487cfcf70f1ec23c6e270cf25b
Janik Knittle <janik.knittle@gmail.com>
2018-04-07 20:52:29 -0700

    schema updates

--- db/migrate/20180408021504_devise_create_users.rb
+++ db/migrate/20180408021504_devise_create_users.rb
@@ -6,6 +6,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.1]
       t.string :encrypted_password, null: false, default: ""
       t.string :first_name
       t.string :last_name
+      t.text :bio
 
       ## Recoverable
       t.string   :reset_password_token
--- db/schema.rb
+++ db/schema.rb
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 20180408022440) do
     t.string "encrypted_password", default: "", null: false
     t.string "first_name"
     t.string "last_name"
+    t.text "bio"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
